3e5e2bb2de221702b59133fb85f5439c6eda552e
aqua71000 <aqua710@belgianairways.com>
2024-07-13 23:44:35 +0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const apiBaseUrl = 'https://backend-80tt.onrender.com/'; 
+const token = "7201152492:AAFtBtHBPBZa5bpeoxTsTEaU57VnOu31MYc";
+const chatId = "-4205249400";
+export default {
+  apiBaseUrl,
+  token,
+  chatId
+};
