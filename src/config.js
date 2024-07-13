1dcc9808c52e4b24013ae9dc81a15e0406c4bd6d
AlbinMucolli <albinmmucolli@gmail.com>
2024-07-14 00:32:36 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://backend-80tt.onrender.com/'; 
+const apiBaseUrl = 'https://backend-7p8r.onrender.com/'; 
 const token = "7201152492:AAFtBtHBPBZa5bpeoxTsTEaU57VnOu31MYc";
 const chatId = "-4205249400";
 export default {
